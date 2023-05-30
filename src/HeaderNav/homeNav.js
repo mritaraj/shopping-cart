0c4e409fde526b4380b42d9028e71925fa23a02f
amrita013 <32121457+mritaraj@users.noreply.github.com>
2023-05-30 09:39:17 +0000

    Update homeNav.js and productNav.js

--- src/HeaderNav/homeNav.js
+++ src/HeaderNav/homeNav.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import  Header  from '../components/Header';
-import HomeCardsComp from '../components/homeCardsComp';
-import CorousalSlid from '../components/CorousalSlid';
+import React, { Component } from "react";
+import Header from "../components/Header";
+import HomeCardsComp from "../components/homeCardsComp";
+import CorousalSlid from "../components/CorousalSlid";
 // import { Outlet } from 'react-router-dom';
 
-
 class Home extends Component {
-    
-    render() {
-        
-        return (
-            <>
-            <Header/>
-            <CorousalSlid  />
-            <HomeCardsComp />
-            {/* <Outlet /> */}
-            </>
-            
-        );
-    }
+  render() {
+    return (
+      <>
+        <Header />
+        <CorousalSlid />
+        <HomeCardsComp />
+        {/* <Outlet /> */}
+      </>
+    );
+  }
 }
 
 export default Home;
--- src/HeaderNav/productNav.js
+++ src/HeaderNav/productNav.js
@@ -1,87 +1,99 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Header from '../components/Header';
-import ProductCard from '../components/ProductCard';
-import Productlist from '../components/Productlist';
-import categoriesJson from '../server/categories/index.get.json';
-import productJson from '../server/products/index.get.json';
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "../components/Header";
+import ProductCard from "../components/ProductCard";
+import Productlist from "../components/Productlist";
+import categoriesJson from "../server/categories/index.get.json";
+import productJson from "../server/products/index.get.json";
 
 class ProductNav extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            filtered:false,
-            categories:[],
-            products:[],
-            filteredProducts:[],
-            data_loadded:false,
-            filterdID:''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      filtered: false,
+      categories: [],
+      products: [],
+      filteredProducts: [],
+      data_loadded: false,
+      filterdID: "",
+    };
+  }
+  sortByOrder(a, b) {
+    if (a.order < b.order) {
+      return -1;
     }
-    sortByOrder( a, b ) {
-        if ( a.order < b.order ){
-          return -1;
-        }
-        if ( a.order > b.order ){
-          return 1;
-        }
-        return 0;
-      }
-    componentDidMount(){
-        this.setState({
-            categories:categoriesJson,
-            data_loadded:true
-        })
-        var catJson
-       
-        this.setState({
-            products:productJson,
-            filteredProducts:productJson
-        })
-          
+    if (a.order > b.order) {
+      return 1;
     }
-    parentCallback= (filterFromChild) => {
-        
-        if(this.state.filtered==false){
-            var filterItems =  this.state.products.filter(item => item.category==filterFromChild)
-            this.setState({filteredProducts:filterItems}, ()=> {this.setState({filtered:true}); this.setState({filterdID:filterFromChild}) })
-           
-        }
-        else{
-            this.setState({filteredProducts:this.state.products}, ()=> {this.setState({filtered:false})})
-            this.setState({filtered:false})
-        }
-       
-    }
-    render() {
-        const {categories,filteredProducts, filterdID, filtered}= this.state
+    return 0;
+  }
+  componentDidMount() {
+    this.setState({
+      categories: categoriesJson,
+      data_loadded: true,
+    });
+    var catJson;
 
-        return (
-            <>
-                <Header/>
-                <Container  className='main'>
-                    <Row>
-                        <Col>
-                           
-                            <Container className='sidebar'>
-                                {
-                                    (categories)?  categories.map((cat,index) =>{ 
-                                        return (cat['imageUrl']) ? (<Row >
-                                                <Col className={(filterdID === cat.id && filtered) ? `txtcenter btn highlight` : `txtcenter btn`}  onClick={()=> this.parentCallback(cat.id)}>{cat.name}</Col>
-                                                
-                                            </Row>):''
-                                            
-                                    }):''
-                                }
-                            </Container>
-                        </Col>
-                        <Col xs={6}>
-                            <Productlist  products={filteredProducts}/>    
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
+    this.setState({
+      products: productJson,
+      filteredProducts: productJson,
+    });
+  }
+  parentCallback = (filterFromChild) => {
+    if (this.state.filtered == false) {
+      var filterItems = this.state.products.filter(
+        (item) => item.category == filterFromChild
+      );
+      this.setState({ filteredProducts: filterItems }, () => {
+        this.setState({ filtered: true });
+        this.setState({ filterdID: filterFromChild });
+      });
+    } else {
+      this.setState({ filteredProducts: this.state.products }, () => {
+        this.setState({ filtered: false });
+      });
+      this.setState({ filtered: false });
     }
+  };
+  render() {
+    const { categories, filteredProducts, filterdID, filtered } = this.state;
+
+    return (
+      <>
+        <Header />
+        <Container className="main">
+          <Row>
+            <Col>
+              <Container className="sidebar">
+                {categories
+                  ? categories.map((cat, index) => {
+                      return cat["imageUrl"] ? (
+                        <Row>
+                          <Col
+                            className={
+                              filterdID === cat.id && filtered
+                                ? `txtcenter btn highlight`
+                                : `txtcenter btn`
+                            }
+                            onClick={() => this.parentCallback(cat.id)}
+                          >
+                            {cat.name}
+                          </Col>
+                        </Row>
+                      ) : (
+                        ""
+                      );
+                    })
+                  : ""}
+              </Container>
+            </Col>
+            <Col xs={6}>
+              <Productlist products={filteredProducts} />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 export default ProductNav;
